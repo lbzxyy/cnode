751eda7b69601f620092acfa0eb83a5f0bcdd546
leepoo <leepoo2008@163.com>
2019-11-15 15:43:20 +0800

    使用react hook 写法完成 菜单栏头部 样式 和交互

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,21 +1,41 @@
-import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
+import Taro, { useState } from '@tarojs/taro'
+import { View , Text} from '@tarojs/components'
+import { AtDrawer, AtIcon } from 'taro-ui'
+import './menu.scss'
+function Menu() {
+  
+  const [show, setShow] = useState(false)
+  
 
-class menu extends Component {
-
-  componentWillMount () { }
-
-  componentDidMount () { }
-
-  render () {
-    const prefixCls = 'ehome-index'
+  const openDrawer = () => {
+    setShow(true)
+  }
 
     return (
-      <View className={prefixCls}>
-
+      <View>
+         <View className='topiclist-menu'>
+       
+          <AtIcon 
+          value='menu' 
+          size='30' 
+          onClick={openDrawer}
+          ></AtIcon>
+          <Text>全部</Text>
+          <AtIcon value='user' size='30' className='image  right'></AtIcon>
+          
+        </View>
+        <View>
+          <AtDrawer 
+            style='position:absolute;'
+            show={ show } 
+            mask 
+            // onClose={this.onClose.bind(this)} 
+            items={['菜单1', '菜单2']}
+          ></AtDrawer>
+        </View>
       </View>
+     
     )
-  }
 }
 
-export default menu
+export default Menu
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -2,6 +2,7 @@ import Taro, { Component } from '@tarojs/taro'
 import { View, Button, Text } from '@tarojs/components'
 import './index.scss'
 import { AtButton } from 'taro-ui'
+import Menu from '../../components/menu/menu'
 
 class Index extends Component {
 
@@ -22,8 +23,7 @@ class Index extends Component {
   render () {
     return (
       <View className='index'>
-        <View><Text>Hello, World</Text></View>
-        <View><AtButton type="primary">hello Taro</AtButton></View>
+        <Menu />
       </View>
     )
   }
